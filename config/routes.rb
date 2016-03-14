fe40ca1debd527b8ebf4e9021849ae8021ba98f7
Thong Dinh <tdinh18@berkeley.edu>
2016-03-14 02:37:26 -0700

    almost done

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root to: 'home#index'
   devise_for :trainers
   resources :trainers
+  patch 'capture', to: 'pokemons#capture', as: 'capture'
+  patch 'damage', to: 'pokemons#damage', as: 'damage'
+  get 'pokemons/new', to: 'pokemons#new', as: 'new'
+  post 'pokemons', to: 'pokemons#create'
+
 end
